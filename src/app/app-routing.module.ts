fcd79636ffeb58cf5a50e9d626c3f4b80a25ed66
Andres <andres.torcuato93@hotmail.com>
2024-10-24 11:12:59 +0200

    feat: :construction: initial project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: 'progress', component: ProgressComponent },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
+    ]
+  },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NoPageFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
